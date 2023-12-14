d75531e91d2b169933e66efa714ef2dce7ce11b7
azran <150814745+azrankabirann@users.noreply.github.com>
2023-12-14 21:48:30 +0700

    Add files via upload

---
+++ src/Components/Mapel/Senin.jsx
@@ -0,0 +1,37 @@
+const Senin = () => {
+    return (
+        <>
+        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Matematika</div>
+					<div className="flex justify-center items-center text-sm">08.20-08.55</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Tajwid</div>
+					<div className="flex justify-center items-center text-sm">08.55-09.30</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">09.30-10.10</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+					<div className="w-[50%] text-base  font-medium">Bahasa Ingris</div>
+					<div className="flex justify-center items-center text-sm">10.10-11.20</div>
+				</div>
+
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Qur'an Hadist</div>
+					<div className="flex justify-center items-center text-sm">11.20-12.30</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">12.30-13.10</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">Pkn</div>
+					<div className="flex justify-center items-center text-sm">13.10-13.45</div>
+				</div>
+        </>
+    )
+} 
+
+export default Senin;
