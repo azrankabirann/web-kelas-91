f6a4ec046d03175dd9f8d795ff010c7c5a90e2ec
azran <150814745+azrankabirann@users.noreply.github.com>
2023-12-15 07:03:05 +0700

    Add files via upload

---
+++ src/components/BoxOldWeb.jsx
@@ -0,0 +1,16 @@
+
+const BoxOldWeb = () => {
+	return (
+		<div id="BoxOldWeb">
+			<a href="https://open.spotify.com/playlist/0HuvJl8Og0pAIZa3pgDkXx?si=z3hccKYDQpys7Fo-rkhEFQ">
+				<div className="flex justify-between">
+					<img src="/link.png" alt="" className="w-auto h-6" />
+					<img src="/next.png" alt="" className="h-3 w-3" />
+				</div>
+				<h1 className="text-white text-base font-semibold pr-0 mt-5">PlayList Abu</h1>
+			</a>
+		</div>
+	)
+}
+
+export default BoxOldWeb
