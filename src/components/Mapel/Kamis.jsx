d1f1db51d0217806cc2eda92014467e19b856d21
azran <150814745+azrankabirann@users.noreply.github.com>
2023-12-15 07:05:09 +0700

    Add files via upload

---
+++ src/components/Mapel/Kamis.jsx
@@ -0,0 +1,64 @@
+const Kamis = () => {
+	return (
+		<>
+			<div
+				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="600">
+				<div className="w-[50%] text-base  font-medium">Bahasa Indonesia</div>
+				<div className="flex justify-center items-center text-sm">07.45-08.20</div>
+			</div>
+			<div
+				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+				data-aos="fade-up"
+				data-aos-duration="600">
+				<div className="w-[50%] text-base  font-medium">Bahasa Ingris</div>
+				<div className="flex justify-center items-center text-sm">08.20-08.55</div>
+			</div>
+			<div
+				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="700">
+				<div className="w-[50%] text-base  font-medium">Ips</div>
+				<div className="flex justify-center items-center text-sm">08.55-09.30</div>
+			</div>
+			<div
+				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="800">
+				<div className="w-[50%] text-base  font-medium">Istirahat</div>
+				<div className="flex justify-center items-center text-sm">09.30-10.10</div>
+			</div>
+			<div
+				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="1000">
+				<div className="w-[50%] text-base font-medium">Tilawah</div>
+				<div className="flex justify-center items-center text-sm">10.10-10.45</div>
+			</div>
+			<div
+				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="1000">
+				<div className="w-[50%] text-base font-medium">Ski</div>
+				<div className="flex justify-center items-center text-sm">10.45-11.55</div>
+			</div>
+			<div
+				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="1000">
+				<div className="w-[50%] text-base font-medium">Bahasa Arab</div>
+				<div className="flex justify-center items-center text-sm">11.55-13.45</div>
+			</div>
+			<div
+				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="1000">
+				<div className="w-[50%] text-base font-medium">Istirahat</div>
+				<div className="flex justify-center items-center text-sm">12.30-13.10</div>
+			</div>
+		</>
+	)
+}
+
+export default Kamis
