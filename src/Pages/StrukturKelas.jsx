f61d80688b6c2d03ea8043d47a8be723f0bb5a63
azran <150814745+azrankabirann@users.noreply.github.com>
2024-07-19 16:54:56 +0700

    Update StrukturKelas.jsx

--- src/Pages/StrukturKelas.jsx
+++ src/Pages/StrukturKelas.jsx
@@ -18,7 +18,7 @@ const StrukturKelas = () => {
 		<div className="z-1  relative h-auto lg:h-[83vh] lg:overflow-hidden">
 			{/* wali kelas */}
 			<div data-aos="fade-up" data-aos-duration="500" className="mt-14 md:mt-10">
-				<BorderStruktur Jabatan="Wali Kelas" Nama="Ayu Singgih S.OS" Width="150px" />
+				<BorderStruktur Jabatan="Wali Kelas" Nama="M.Azran Kabiran" Width="150px" />
 			</div>
 			<div className="flex flex-col justify-center items-center">
 				<img src="LineVertikal.svg" alt="" data-aos="fade-up" data-aos-duration="550" />
